1e98448603ff77083187a1ac049e932c20abbc29
vlydikmEtal <153929639+vlydikmEtal@users.noreply.github.com>
2025-08-10 12:32:56 +0300

    merge4

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,8 +1,15 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  inject,
+  ElementRef,
+  Renderer2,
+  HostListener,
+} from '@angular/core';
 import { ProfileService } from '../../data/services/profile.service';
 import { ProfileCardComponent } from '../../common-ui/profile-card/profile-card.component';
 import { ProfileFiltersComponent } from './profile-filters/profile-filters.component';
 import { CommonModule } from '@angular/common';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-search-page',
@@ -13,7 +20,29 @@ import { CommonModule } from '@angular/common';
 })
 export class SearchPageComponent {
   ProfileService = inject(ProfileService);
-  profiles = this.ProfileService.filteredProfiles
+  profiles = this.ProfileService.filteredProfiles;
 
-  constructor() {}
+  hostElement = inject(ElementRef);
+  r2 = inject(Renderer2);
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.resizeFeed();
+  }
+
+  ngAfterViewInit() {
+    this.resizeFeed();
+
+    fromEvent(window, 'resize')
+      .subscribe(() => {
+        console.log(123)
+      })
+  }
+
+  resizeFeed() {
+    const { top } = this.hostElement.nativeElement.getBoundingClientRect();
+
+    const height = window.innerHeight - top - 24 - 24;
+    this.r2.setStyle(this.hostElement.nativeElement, 'height', `${height}px`);
+  }
 }
